a57efd0fb9a14b677371c3f203930bffcc9001bf
John Nabil <johnnabil85@gmail.com>
2020-11-13 21:16:31 +0200

    Initial Commit

---
+++ tictac/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tictac
+//
+//  Created by John Nabil Takla on 13/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
